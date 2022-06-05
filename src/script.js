98be7c95f5e047cf92171714daa7278662551bb3
TomiRenaldi <tomirenaldi2@gmail.com>
2022-06-06 01:19:40 +0700

    :sparkles: Add robot > Improve lights

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js'
 
 /**
  * Base
@@ -10,6 +13,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+RectAreaLightUniformsLib.init()
 
 /**
  * Sizes
@@ -39,23 +43,56 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
+camera.position.set(0, 5, - 30)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
+// Lights
+const rectAreaLights = new THREE.RectAreaLight('#ff00cc', 50, 4, 10)
+rectAreaLights.position.set(- 5, 5, 5)
+scene.add(rectAreaLights)
+
+const rectAreaLights1 = new THREE.RectAreaLight('#ff00cc', 50, 4, 10)
+rectAreaLights1.position.set(0, 5, 5)
+scene.add(rectAreaLights1)
+
+const rectAreaLights2 = new THREE.RectAreaLight('#ff00cc', 50, 4, 10)
+rectAreaLights2.position.set(5, 5, 5)
+scene.add(rectAreaLights2)
+
+scene.add(new RectAreaLightHelper(rectAreaLights))
+scene.add(new RectAreaLightHelper(rectAreaLights1))
+scene.add(new RectAreaLightHelper(rectAreaLights2))
+
 /**
- * Cube
+ * Robots
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(cube)
+const robots = {}
+
+robots.geometry = new THREE.BoxGeometry(100, 0.1, 100)
+
+robots.material = new THREE.MeshStandardMaterial({ 
+    color: 0x080808,
+    metalness: 0
+})
+
+robots.mesh = new THREE.Mesh(robots.geometry, robots.material)
+robots.mesh.receiveShadow = true
+scene.add(robots.mesh)
+
+robots.loader = new GLTFLoader()
+robots.loader.load('Soldier.glb', (gltf) => {
+    const model = gltf.scene
+    scene.add(model)
+    gltf.scene.traverse((child) => {
+        if(child instanceof THREE.Mesh) {
+            child.castShadow = true
+        }
+    })
+})
 
 /**
  * Renderer
@@ -64,6 +101,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true,
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
